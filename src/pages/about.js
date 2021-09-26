edbc3c02139226a662c6e31ab90660df0732dffe
Arris Le <hang.le.3329@gmail.com>
2021-09-26 20:16:18 +0700

    hahahahahahahahaha

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+// Step 1: Import React
+import * as React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle=":D">
+      <h1>BLIN!</h1>
+      <p>TO THE BLIN AND BEYOND!</p>
+      </Layout>
+    </main>
+  )
+}
+// Step 3: Export your component
+export default AboutPage
